053254b963621be1373a19df30a66a45c529ff50
narrativityy <89665383+narrativityy@users.noreply.github.com>
2022-11-22 23:33:50 -0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+package cs141.jmcrowley.Project.HannahFiles;
+
 public class Student {
     public int id;
     public String name;
@@ -6,6 +8,8 @@ public class Student {
     public String grade;
     public static int numStudents;
 
+
+    // constructor
     public Student(int id, String name, String course, String teacher, String grade) {
         this.id = id;
         this.name = name;
@@ -14,54 +18,57 @@ public class Student {
         this.grade = grade;
         Student.incrementNumStudents();
     }
+    
+    // print student info
+    public void printStudent() {
+    	System.out.println("Name: " + name);
+    	System.out.println("ID: " + id);
+    	System.out.println("Course: " + course);
+    	System.out.println("Teacher: " + teacher);
+    	System.out.println("Grade: " + grade);
+        System.out.println();
+    }
 
+    
+    
+    
+    // getters & setters
     public int getId() {
         return this.id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return this.name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getCourse() {
         return this.course;
     }
-
     public void setCourse(String course) {
         this.course = course;
     }
-
     public String getTeacher() {
         return this.teacher;
     }
-
     public void setTeacher(String teacher) {
         this.teacher = teacher;
     }
-
     public String getGrade() {
         return this.grade;
     }
-
     public void setGrade(String grade) {
         this.grade = grade;
     }
-
-    public void printStudents() {
+    public static int getNumStudents() {
+    	return numStudents;
     }
-
     public static void incrementNumStudents() {
         ++numStudents;
     }
-
     public static void decrementNumStudents() {
         --numStudents;
     }
